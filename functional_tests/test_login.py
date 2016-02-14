8a8a3c7e557aebe310d0c7f9c634ce9348c65005
Nathan Braun <nathanericbraun@gmail.com>
2016-02-14 15:41:53 -0600

    Logout works.

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -6,8 +6,11 @@ class LoginTest(FunctionalTest):
 
     def wait_for_element_with_id(self, element_id):
         WebDriverWait(self.browser, timeout=30).until(
-                lambda b: b.find_element_by_id(element_id)
-                )
+            lambda b: b.find_element_by_id(element_id),
+            'Could not find element with id {}. Page text was:\n{}'.format(
+                element_id, self.browser.find_element_by_tag_name('body').text
+            )
+        )
 
     def switch_to_new_window(self, text_in_title):
         retries = 60
@@ -19,6 +22,18 @@ class LoginTest(FunctionalTest):
             retries -= 1
             time.sleep(0.5)
         self.fail('could not find window')
+
+
+    def wait_to_be_logged_in(self):
+        self.wait_for_element_with_id('id_logout')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn('edith@mockmyid.com', navbar.text)
+
+
+    def wait_to_be_logged_out(self):
+        self.wait_for_element_with_id('id_login')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn('edith@mockmyid.com', navbar.text)
 
 
     def test_login_with_persona(self):
@@ -42,7 +57,17 @@ class LoginTest(FunctionalTest):
         self.switch_to_new_window('To-Do')
 
         # She can see that shee is logged in
-        self.wait_for_element_with_id('id_logout')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn('edith@mockmyid.com', navbar.text)
-        
+        self.wait_to_be_logged_in()
+
+        # Refreshing the page, she sees it's a real session login, not just a
+        # one-off for that page
+        self.browser.refresh()
+        self.wait_to_be_logged_in()
+
+        # She logs out
+        self.browser.find_element_by_id('id_logout').click()
+        self.wait_to_be_logged_out()
+
+        # logged out persists after refresh
+        self.browser.refresh()
+        self.wait_to_be_logged_out()
